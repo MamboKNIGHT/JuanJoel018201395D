a040e1a0d9f1921aea2b61987a7b9a06ca624020
MamboKNIGHT <018201395d@uandina.edu.pe>
2022-08-08 16:22:37 -0500

    Initial commit

---
+++ src/main/java/com/example/juanjoel018201395d/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.juanjoel018201395d;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Bienvenido a la Aplicación JavaFX!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.juanjoel018201395d {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.juanjoel018201395d to javafx.fxml;
+    exports com.example.juanjoel018201395d;
+}
